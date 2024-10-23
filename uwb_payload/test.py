20d41a3e1f27c8cdb3d89f0e810221b176e11b4c
sagar-sadak <sadaksagar9@gmail.com>
2024-10-23 11:35:13 -0400

    x

--- uwb_payload/test.py
+++ uwb_payload/test.py
@@ -1,10 +1,13 @@
 import serial
 import threading
 import time
+import asyncio
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from sphero_sdk import SpheroRvrObserver
+from sphero_sdk import SpheroRvrObserver, SpheroRvrAsync, SerialAsyncDal
+# loop = asyncio.get_event_loop()
+# rvr = SpheroRvrAsync(dal=SerialAsyncDal)
 
 # Configure your serial port and baud rate
 SERIAL_PORT = '/dev/ttyACM0'  # Adjust this according to your setup
@@ -58,29 +61,29 @@ def process_data():
         #         print("Unkown exception occurred while driving forward")
 
 def main():
-    # serial_thread = threading.Thread(target=read_serial)
+    serial_thread = threading.Thread(target=read_serial)
     # processing_thread = threading.Thread(target=process_data)
 
-    # serial_thread.start()
+    serial_thread.start()
     # processing_thread.start()
 
     # serial_thread.join()
     # processing_thread.join()
-    global serial_data
-    delim = " "
-    try:
-        with serial.Serial(SERIAL_PORT, BAUD_RATE) as ser:
-            while True:
-                ser.reset_input_buffer()
-                # while ser.in_waiting > 50:
-                #     _ = ser.readline()
-                line = ser.readline().decode('utf-8').split(delim)
-                # print(f"Received: {line}")
-                print(line)
-                serial_data = (int) (line[2])
-                # time.sleep(0.5)
-    except serial.SerialException as e:
-        print(f"Serial exception: {e}")
+    # global serial_data
+    # delim = " "
+    # try:
+    #     with serial.Serial(SERIAL_PORT, BAUD_RATE) as ser:
+    #         while True:
+    #             ser.reset_input_buffer()
+    #             # while ser.in_waiting > 50:
+    #             #     _ = ser.readline()
+    #             line = ser.readline().decode('utf-8').split(delim)
+    #             # print(f"Received: {line}")
+    #             print(line)
+    #             serial_data = (int) (line[2])
+    #             # time.sleep(0.5)
+    # except serial.SerialException as e:
+    #     print(f"Serial exception: {e}")
 
 if __name__ == "__main__":
     main()
